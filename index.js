137b357fae9292328afcf7341f017ec1f9633305
jayplin <teSvDPG77VVx9KR>
2019-09-05 09:38:49 +0200

    change app.listen medthod to not only localhost

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const db = require("./queries");
-const port = 3000;
+const cors = require("cors");
 
+//app.use(cors);
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
 );
-
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 //default route
 app.get("/", (request, response) => {
   response.json({ info: "Node.js, Express, and Postgres API" });
@@ -22,6 +28,6 @@ app.post("/pomodoros", db.addPomodoro);
 app.put("/pomodoros/:id", db.updatePomodoro);
 app.delete("/pomodoros/:id", db.deletePomodoro);
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`);
+app.listen(process.env.PORT || 3002, () => {
+  console.log(`Server listening`);
 });
